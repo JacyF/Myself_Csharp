ef6bd6587e31ef08b34cf9754b357ea632bb5839
JacyF <brunoloouuiis@gmail.com>
2022-12-05 01:46:43 +0300

    added004

---
+++ DECEMBER_2022/004/Program.cs
@@ -0,0 +1,21 @@
+﻿// 4. Write a C# Sharp program to print the result of the specified operations. 
+// Test data:
+
+// -1 + 4 * 6
+// ( 35+ 5 ) % 7
+// 14 + -4 * 6 / 11
+// 2 + 15 / 6 * 1 - 7 % 2
+// Expected Output:
+// 23
+// 5
+// 12
+// 3
+
+Console.WriteLine(" printing the result of specified operations ");
+
+Console.WriteLine($" 1 -->   -1 + 4 * 6 = {-1 + 4 * 6} ");
+Console.WriteLine($" 2 -->   ( 35+ 5 ) % 7 = {(35 + 5) % 7} ");
+Console.WriteLine($" 3 -->   14 + -4 * 6 / 11 = {14 + -4 * 6 / 11} ");
+Console.WriteLine($" 4 -->   2 + 15 / 6 * 1 - 7 % 2 = {2 + 15 / 6 * 1 - 7 % 2} ");
+
+
