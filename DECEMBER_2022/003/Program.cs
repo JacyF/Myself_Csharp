92a533f0ff1a3b6f341404c2d9dc6abc40a60e8c
JacyF <brunoloouuiis@gmail.com>
2022-12-05 01:39:20 +0300

    added003

---
+++ DECEMBER_2022/003/Program.cs
@@ -0,0 +1,10 @@
+﻿// 3. Write a C# Sharp program to print the result of dividing two numbers.
+
+Console.WriteLine(" Please, introduce 2 numbers ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Please, introduce one more number ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+
+double div = number1 / number2 ;
+Console.WriteLine($" {number1} / {number2} = {div} ");
