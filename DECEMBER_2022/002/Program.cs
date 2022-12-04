b86eb815513f36f2a3c01657fa3d2db5ffab7df1
JacyF <brunoloouuiis@gmail.com>
2022-12-05 01:35:32 +0300

    ADDED002

---
+++ DECEMBER_2022/002/Program.cs
@@ -0,0 +1,11 @@
+﻿// 2. Write a C# Sharp program to print the sum of two numbers.
+
+Console.WriteLine(" Please, introduce 2 numbers ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Please, introduce one more number ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+
+double sum = number1 + number2 ;
+
+Console.WriteLine($" {number1} + {number2} = {sum} ");
