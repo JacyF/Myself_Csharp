304ab6b7de0ff451398c683d560cc7f2d5e41e6d
JacyF <brunoloouuiis@gmail.com>
2022-12-05 01:52:36 +0300

    added005

---
+++ DECEMBER_2022/005/Program.cs
@@ -0,0 +1,19 @@
+﻿// 5. Write a C# Sharp program to swap two numbers. 
+// Test Data:
+// Input the First Number : 5
+// Input the Second Number : 6
+// Expected Output: 
+// After Swapping :
+// First Number : 6 
+// Second Number : 5 
+
+Console.WriteLine("Please , introduce to numbers to swap ");
+int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"First number : {first} ");
+Console.WriteLine($"Second number : {second} ");
+
+Console.WriteLine("After swapping ");
+Console.WriteLine($"First number : {second} ");
+Console.WriteLine($"Second number : {first} ");
