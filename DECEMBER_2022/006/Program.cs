8998cd585cfc760609b4a9f89328eb49ca3e9a9a
JacyF <brunoloouuiis@gmail.com>
2022-12-05 03:20:57 +0300

    added 006

---
+++ DECEMBER_2022/006/Program.cs
@@ -0,0 +1,19 @@
+﻿// 6. Write a C# Sharp program to print the output of multiplication of three numbers 
+// which will be entered by the user. 
+// Test Data:
+// Input the first number to multiply: 2
+// Input the second number to multiply: 3
+// Input the third number to multiply: 6
+// Expected Output:
+// 2 x 3 x 6 = 36
+
+Console.Write("Please , introduce the first number to multiply : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please , introduce the second number to multiply : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please , introduce the third number to multiply : ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($" {num1} x {num2} x {num3} = {num1 * num2 * num3} ");
