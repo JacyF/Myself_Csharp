3ffabd3f655a43d809e7599b7d29145f77daf8cf
JacyF <brunoloouuiis@gmail.com>
2022-12-05 03:37:05 +0300

    added008

---
+++ DECEMBER_2022/008/Program.cs
@@ -0,0 +1,18 @@
+﻿// 8. Write a C# Sharp program that takes a number as input and 
+// print its multiplication table. 
+// Test Data:
+// Enter the number: 5
+// Expected Output:
+// 5 * 0 = 0
+// 5 * 1 = 5
+// 5 * 2 = 10 
+// 5 * 3 = 15
+// ....
+// 5 * 10 = 50
+
+Console.Write("Please , introduce the first number  : ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine($" {num} x {i} = {num * i} ");
+}
