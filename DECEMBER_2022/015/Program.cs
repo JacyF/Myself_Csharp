0ff0466692179d428e7f5aa65c71b8dbca1f9b23
JacyF <brunoloouuiis@gmail.com>
2023-01-04 23:46:34 +0300

    task 16 redoing

---
+++ DECEMBER_2022/015/Program.cs
@@ -0,0 +1,19 @@
+﻿// 15. Write a C# program remove specified a character from a non-empty string 
+// using index of a character. 
+// Test Data:
+// w3resource
+// Sample Output:
+// wresource
+// w3resourc
+// 3resource
+
+string word = "w3resource" ;
+
+string remove_char(string letter, int n)
+{
+    return letter.Remove(n, 1);
+} 
+
+Console.WriteLine(remove_char(word, 1));
+Console.WriteLine(remove_char(word, 9));
+Console.WriteLine(remove_char(word, 0));
---
+++ DECEMBER_2022/016/Program.cs
@@ -0,0 +1,39 @@
+﻿// 16. Write a C# program to create a new string from a given string 
+// where the first and last characters will change their positions. 
+// Test Data:
+// w3resource
+// Python
+// Sample Output:
+// e3resourcw
+// nythoP 
+// x 
+
+string[] array = { " Python " };
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+}
+
+void FirstLast (string [] arr)
+{
+    string last = "n";
+    string first = "P";
+    for (int i = 0 ; i < arr.Length ; i++)
+    {
+        if (i == 0) arr[i] = last ;
+        // if (i == arr.Length ) arr[i] = first;
+        Console.Write($"{arr[i]}, {i}");
+    }
+}
+
+
+PrintArray(array);
+Console.WriteLine("-------------");
+FirstLast(array);
+PrintArray(array);
+
+
