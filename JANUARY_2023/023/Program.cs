9943138d64fbf77ac4d1ecc52972fef4bc4b174e
JacyF <brunoloouuiis@gmail.com>
2023-01-06 00:52:12 +0300

    added 023

---
+++ JANUARY_2023/023/Program.cs
@@ -0,0 +1,6 @@
+﻿// 23. Write a C# program to convert a given string into lowercase. 
+// Sample Output:
+// write a c# sharp program to display the following pattern using the alphabet.
+
+string word =  " BRUNO a c# sharp program to display the following pattern using the alphabet.";
+Console.Write(word.ToLower());
