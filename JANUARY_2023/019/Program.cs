d2506348063d42d30f8569a5061d2faa0497ff0a
JacyF <brunoloouuiis@gmail.com>
2023-01-05 23:50:44 +0300

    added 019

---
+++ JANUARY_2023/019/Program.cs
@@ -0,0 +1,17 @@
+﻿// 19. Write a C# program to compute the sum of two given integers, 
+// if two values are equal then return the triple of their sum.
+
+Console.WriteLine("Please introduce two numbers ");
+Console.Write("Fisrt number : ");
+int firstnum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Fisrt number : ");
+int secondnum = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+if (firstnum == secondnum) sum = 3*(firstnum+secondnum);
+else sum = (firstnum+secondnum);
+
+Console.Write($" Sum = {sum} ");
+
