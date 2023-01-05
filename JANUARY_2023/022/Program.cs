ce48ebb4930143555a4357e726cbd0b6c573dec9
JacyF <brunoloouuiis@gmail.com>
2023-01-06 00:30:58 +0300

    added 020

---
+++ JANUARY_2023/022/Program.cs
@@ -0,0 +1,20 @@
+﻿// 22. Write a C# program to check if an given integer is within 20 of 100 or 200. 
+// Sample Output:
+// Input an integer: 
+// 25 
+// False 
+
+Console.Write("Please introduce a number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Within20Of100Or200(int num)
+{
+    if (Math.Abs(num - 100) <= 20 || Math.Abs(num - 200) <= 20) return true;
+    return false;
+}
+
+bool within20Of100Or200 = Within20Of100Or200(number);
+Console.WriteLine(within20Of100Or200);
+
+
+
