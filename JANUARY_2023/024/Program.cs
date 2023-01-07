fdc1ea7dcec86c5aada68cee2179e7ba0fb099a7
JacyF <brunoloouuiis@gmail.com>
2023-01-07 13:42:14 +0300

    added 024

---
+++ JANUARY_2023/024/Program.cs
@@ -0,0 +1,24 @@
+﻿// 24. Write a C# program to find the longest word in a string. Go to the editor
+// Test Data: Write a C# Sharp Program to display the following pattern using the alphabet.
+// Sample Output:
+// following
+
+string line = " Write a C# Sharp Program to display the following pattern using the alphabet. ";
+string [] words = line.Split(new [] {" "}, StringSplitOptions.None);
+// string[] words = { "Write", "a", "C#", "Sharp", "Program:",
+//                     " to", "display", "the" ,"following" ,"pattern",
+//                     "using", "the", "alphabet" };
+string word = " ";
+int ctc = 0;
+
+foreach (String s in words)
+{
+    if (s.Length > ctc)
+    {
+        word = s;
+        ctc = s.Length;
+    }
+}
+
+Console.Write(word);
+
