1958d739964ab3b1ec51d77b5d3c4f43b4eddcfd
JacyF <brunoloouuiis@gmail.com>
2023-01-07 21:02:06 +0300

    added 026

---
+++ JANUARY_2023/026/Program.cs
@@ -0,0 +1,26 @@
+﻿// 26. Write a C# program to compute the sum of the first 500 prime numbers.
+// Sample Output:
+// Sum of the first 500 prime numbers: 
+// 824693 
+
+Console.Write(" The sum of the first 500 prime numbers : ");
+
+int sum = 0;
+int counter = 0;
+
+for (int i = 2; i <= 5000; i++)
+{
+    int count = 0;
+    for (int j = 1; j <= i; j++)
+    {
+        if (i % j == 0) count++;
+    }
+    if (count == 2)
+    {
+        sum = sum + i;
+        counter += 1;
+    }
+    if (counter == 500) break;
+}
+Console.WriteLine(sum);
+Console.WriteLine($" Quantity of numebers summed : {counter}");
