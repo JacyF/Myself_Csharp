1c758dbcd7ad29e9637dd57bd48c8183ab104637
JacyF <brunoloouuiis@gmail.com>
2023-01-07 14:53:48 +0300

    added 025

---
+++ JANUARY_2023/025/Program.cs
@@ -0,0 +1,30 @@
+﻿// 25. Write a C# program to print the odd numbers from 1 to 99. Prints one number per line. 
+// Sample Output:
+// Odd numbers from 1 to 99. Prints one number per line.
+// 1 
+// 3 
+// 5 
+// 7 
+// 9 
+// ...
+// 95
+// 97 
+// 99
+
+Console.WriteLine("Printing the odd numbers from 1 to 99");
+
+int count = 1;
+for (int i = 1; i < 1000; i = i + 2)
+{
+    if (count < 11)
+    {
+        Console.Write($"{i} ");
+        count++;
+    }
+    if (count >= 11)
+    {
+        Console.WriteLine();
+        count = 1;
+    }
+}
+
