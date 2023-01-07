9002d1e4d43d8b3a66dc7d61236623174f478753
JacyF <brunoloouuiis@gmail.com>
2023-01-07 21:37:32 +0300

    added 027

---
+++ JANUARY_2023/027/Program.cs
@@ -0,0 +1,21 @@
+﻿// 27. Write a C# program and compute the sum of the digits of an integer. 
+// Sample Output:
+// Input a number(integer): 12
+// Sum of the digits of the said integer: 3 
+
+Console.Write(" Write a number : ");
+// int number = Convert.Toint32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumdigits = 0 ;
+ int control = number;
+
+while( number > 0)
+{
+    control = number;
+    number = number % 10 ;
+    sumdigits = sumdigits + number;
+    number = control/10;
+}
+
+Console.WriteLine($" Sum of the digits of the said integer: {sumdigits}");
